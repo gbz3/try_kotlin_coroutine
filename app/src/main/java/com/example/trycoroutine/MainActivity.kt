72799234c4d947a744b6660161ebd726f2cdf28f
gbz3 <1808483+gbz3@users.noreply.github.com>
2020-08-22 19:08:26 +0900

    initial commit

---
+++ app/src/main/java/com/example/trycoroutine/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.trycoroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
